9fdb04d2b35bff8c534ce4496a2a0c4113252fc1
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-22 16:50:03 +0600

    Trying typescript with different events

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import List from "./components/List";
 import UserItem from "./components/UserItem";
 import TodoItem from "./components/TodoItem";
+import EventsExample from "./components/EventsExample";
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([]);
@@ -35,6 +36,7 @@ const App = () => {
 
     return (
         <div>
+            <EventsExample/>
             <Card width="200px" height="200px" variant={CardVariant.outlined}>
                 <button>This is button</button>
             </Card>
